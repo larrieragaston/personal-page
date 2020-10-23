6e94a5f1fb2ab9bd8c3e1e844644bb8fb5e65079
facustrauch <facustrauch@gmail.com>
2020-10-23 12:47:23 -0300

    esta todo menos el menu en celulares que esta en display none. Hay que sumarle el js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,7 @@ import React from "react";
 import * as ArgFlag from '../assets/Argentina_Flag.png';
 import * as UKFlag from '../assets/UnitedKindom_Flag.png';
 import ArrowNav from '../assets/arrownav.svg';
+import MenuIcon from '../assets/menu_icon.svg';
 import { translate } from "react-translate";
 
 function NavBar(props) {
@@ -12,7 +13,8 @@ function NavBar(props) {
         <div className="navbar-logo">
           <a className="logohover" href="#about"><h2 className="logo">Gaston A. Larriera</h2></a>
         </div>
-        <ul className="navbar-links">
+        <button id="boton_menu" class="boton_menu"><img src={MenuIcon} className="menuicon"/></button>
+          <ul className="navbar-links" id="navbar-links">
             <li><a href="#about"><span>{t("about")}</span></a></li> 
             <li><a href="#knowledge"><span>{t("technologies")}</span></a></li> 
             <li><a href="#experience"><span>{t("experience")}</span></a></li> 
@@ -21,16 +23,14 @@ function NavBar(props) {
             <li className="nav_flag">
               <img src={ArgFlag}></img>
               <img src={ArrowNav} alt="Icono de flecha" className="arrow_download"></img>
+                <ul class="dropdown">
+                  <li><a href="#" className="dropdown_flag"><img src={ArgFlag} className="dropdown_flag_img"></img> <p>Español (ES)</p></a></li>
+                  <li><a href="#" className="dropdown_flag"><img src={UKFlag} className="dropdown_flag_img"></img> <p>Inglés (EN)</p></a></li>
+                </ul>
             </li>
-              {/* <ul>
-                  <li><a href="#"><img src={ArgFlag}></img>Español</a></li>
-                  <li><a href="#"><img src={ArgFlag}></img>Ingles</a></li>
-              </ul>
-            </li> */}
-        </ul>
+          </ul>
       </div>
       <script>
-        
       </script>
     </nav>
   );
