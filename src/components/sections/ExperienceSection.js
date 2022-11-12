fc2720570ed564e9e7b11e1635b7d4148c60fc51
Gaston Larriera <larriera.gaston@gmail.com>
2022-11-11 21:29:51 -0300

    Fix & translate Work Experience

--- src/components/sections/ExperienceSection.js
+++ src/components/sections/ExperienceSection.js
@@ -9,87 +9,51 @@ function ExperienceSection(props) {
 			<div className="exp-all">
 				<div className="exp-each left firstelement">
 					<div className="exp-title">
-						<h3>UTN INSPT</h3>
-						<p className="exp-date">Agosto 2019 a la Actualidad</p>
+						<h3>{t("wxp1_place")}</h3>
+						<p className="exp-date">{t("wxp1_period")}</p>
 					</div>
-					<p className="bajada">
-						Docente de materia Programación III (terciario)
-					</p>
-					<p>
-						Diseño, desarrollo e implementación de sistemas con orientación web.
-						Análisis y implementación de nuevas funcionalidades. Mantenimiento
-						de aplicaciones existentes. Corrección de errores.
-					</p>
+					<p className="bajada">{t("wxp1_position")}</p>
+					<p>{t("wxp1_description")}</p>
 				</div>
 				<div className="exp-each right">
 					<div className="exp-title">
-						<h3>UTN INSPT</h3>
-						<p className="exp-date">Agosto 2019 a la Actualidad</p>
+						<h3>{t("wxp2_place")}</h3>
+						<p className="exp-date">{t("wxp2_period")}</p>
 					</div>
-					<p className="bajada">
-						Docente de materia Programación III (terciario)
-					</p>
-					<p>
-						Diseño, desarrollo e implementación de sistemas con orientación web.
-						Análisis y implementación de nuevas funcionalidades. Mantenimiento
-						de aplicaciones existentes. Corrección de errores.
-					</p>
+					<p className="bajada">{t("wxp2_position")}</p>
+					<p>{t("wxp2_description")}</p>
 				</div>
 				<div className="exp-each left">
 					<div className="exp-title">
-						<h3>UTN INSPT</h3>
-						<p className="exp-date">Agosto 2019 a la Actualidad</p>
+						<h3>{t("wxp3_place")}</h3>
+						<p className="exp-date">{t("wxp3_period")}</p>
 					</div>
-					<p className="bajada">
-						Docente de materia Programación III (terciario)
-					</p>
-					<p>
-						Diseño, desarrollo e implementación de sistemas con orientación web.
-						Análisis y implementación de nuevas funcionalidades. Mantenimiento
-						de aplicaciones existentes. Corrección de errores.
-					</p>
+					<p className="bajada">{t("wxp3_position")}</p>
+					<p>{t("wxp3_description")}</p>
 				</div>
 				<div className="exp-each right">
 					<div className="exp-title">
-						<h3>UTN INSPT</h3>
-						<p className="exp-date">Agosto 2019 a la Actualidad</p>
+						<h3>{t("wxp4_place")}</h3>
+						<p className="exp-date">{t("wxp4_period")}</p>
 					</div>
-					<p className="bajada">
-						Docente de materia Programación III (terciario)
-					</p>
-					<p>
-						Diseño, desarrollo e implementación de sistemas con orientación web.
-						Análisis y implementación de nuevas funcionalidades. Mantenimiento
-						de aplicaciones existentes. Corrección de errores.
-					</p>
+					<p className="bajada">{t("wxp4_position")}</p>
+					<p>{t("wxp4_description")}</p>
 				</div>
 				<div className="exp-each left">
 					<div className="exp-title">
-						<h3>UTN INSPT</h3>
-						<p className="exp-date">Agosto 2019 a la Actualidad</p>
+						<h3>{t("wxp5_place")}</h3>
+						<p className="exp-date">{t("wxp5_period")}</p>
 					</div>
-					<p className="bajada">
-						Docente de materia Programación III (terciario)
-					</p>
-					<p>
-						Diseño, desarrollo e implementación de sistemas con orientación web.
-						Análisis y implementación de nuevas funcionalidades. Mantenimiento
-						de aplicaciones existentes. Corrección de errores.
-					</p>
+					<p className="bajada">{t("wxp5_position")}</p>
+					<p>{t("wxp5_description")}</p>
 				</div>
 				<div className="exp-each right">
 					<div className="exp-title">
-						<h3>UTN INSPT</h3>
-						<p className="exp-date">Agosto 2019 a la Actualidad</p>
+						<h3>{t("wxp6_place")}</h3>
+						<p className="exp-date">{t("wxp6_period")}</p>
 					</div>
-					<p className="bajada">
-						Docente de materia Programación III (terciario)
-					</p>
-					<p>
-						Diseño, desarrollo e implementación de sistemas con orientación web.
-						Análisis y implementación de nuevas funcionalidades. Mantenimiento
-						de aplicaciones existentes. Corrección de errores.
-					</p>
+					<p className="bajada">{t("wxp6_position")}</p>
+					<p>{t("wxp6_description")}</p>
 				</div>
 			</div>
 		</section>
