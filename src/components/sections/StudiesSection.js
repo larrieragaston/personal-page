816ec5801704cbf08bc854b9611f0bee060ef8b3
Gaston Larriera <larriera.gaston@gmail.com>
2022-11-11 21:51:21 -0300

    Fix Studies & translate

--- src/components/sections/StudiesSection.js
+++ src/components/sections/StudiesSection.js
@@ -9,126 +9,38 @@ function StudiesSection(props) {
 			<div className="exp-all">
 				<div className="studies-each left firstelement">
 					<div className="exp-title">
-						<h3>Ingeniería Informática</h3>
-						<p className="exp-date">2016- Actualidad</p>
+						<h3>{t("std1_title")}</h3>
+						<p className="exp-date">{t("std1_period")}</p>
 					</div>
-					<p className="bajada">Universidad Argentina de la Empresa (UADE)</p>
+					<p className="bajada">{t("std1_place")}</p>
 				</div>
 				<div className="studies-each right">
 					<div className="exp-title">
-						<h3>
-							Profesorado en Disciplinas Industriales Esp. Informática e
-							Informática Aplicada
-						</h3>
-						<p className="exp-date">2014 - 2018 | Prom. 7,44</p>
+						<h3>{t("std2_title")}</h3>
+						<p className="exp-date">{t("std2_period")}</p>
 					</div>
-					<p className="bajada">Universidad Tecnológica Nacional (UTN)</p>
+					<p className="bajada">{t("std2_place")}</p>
 				</div>
 				<div className="studies-each left">
 					<div className="exp-title">
-						<h3>Tecnicatura Superior en Informática e Informática Aplicada</h3>
-						<p className="exp-date">2011 - 2014 | Prom. 8,63</p>
+						<h3>{t("std3_title")}</h3>
+						<p className="exp-date">{t("std3_period")}</p>
 					</div>
-					<p className="bajada">Universidad Tecnológica Nacional (UTN)</p>
-					<p>
-						Proyecto final: Sistema de Administración de Cursos de Extensión.
-					</p>
+					<p className="bajada">{t("std3_place")}</p>
 				</div>
 				<div className="studies-each right">
 					<div className="exp-title">
-						<h3>Desarrollo de aplicaciones web full-stack con MEAN</h3>
-						<p className="exp-date">Actualmente</p>
+						<h3>{t("std4_title")}</h3>
+						<p className="exp-date">{t("std4_period")}</p>
 					</div>
-					<p className="bajada">
-						Universidad Tecnológica Nacional Bs. As. (UTN FRBA)
-					</p>
+					<p className="bajada">{t("std4_place")}</p>
 				</div>
 				<div className="studies-each left">
 					<div className="exp-title">
-						<h3>
-							Javascript Algorithms And Data Structures Certification (300
-							hours)
-						</h3>
-						<p className="exp-date">2019</p>
+						<h3>{t("std5_title")}</h3>
+						<p className="exp-date">{t("std5_period")}</p>
 					</div>
-					<p className="bajada">Freecodecamp.org</p>
-				</div>
-				<div className="studies-each right">
-					<div className="exp-title">
-						<h3>Responsive Web Design Certification (300 hours)</h3>
-						<p className="exp-date">2019</p>
-					</div>
-					<p className="bajada">Freecodecamp.org</p>
-				</div>
-				<div className="studies-each left">
-					<div className="exp-title">
-						<h3>Fast Track - SAS Visual Analitics</h3>
-						<p className="exp-date">2016</p>
-					</div>
-					<p className="bajada">SAS Argent</p>
-				</div>
-				<div className="studies-each right">
-					<div className="exp-title">
-						<h3>Taller Tunning SQL</h3>
-						<p className="exp-date">2015</p>
-					</div>
-					<p className="bajada">Educación IT</p>
-				</div>
-				<div className="studies-each left">
-					<div className="exp-title">
-						<h3>Introducción al negocio bancario</h3>
-						<p className="exp-date">2015</p>
-					</div>
-					<p className="bajada">UADE Business School</p>
-				</div>
-				<div className="studies-each right">
-					<div className="exp-title">
-						<h3>Operador Básico GNU/Linux</h3>
-						<p className="exp-date">2014</p>
-					</div>
-					<p className="bajada">C.F.P. N° 34 (Sub-sede Pío IX)</p>
-				</div>
-				<div className="studies-each left">
-					<div className="exp-title">
-						<h3>Curso SIU TOBA Nivel inicial</h3>
-						<p className="exp-date">2014</p>
-					</div>
-					<p className="bajada">Sistema de Información Universitaría</p>
-				</div>
-				<div className="studies-each right">
-					<div className="exp-title">
-						<h3>Autoridad docente en los nuevos escenarios educativos</h3>
-						<p className="exp-date">2014</p>
-					</div>
-					<p className="bajada">Universidad Tecnológica Nacional (UTN)</p>
-				</div>
-				<div className="studies-each left">
-					<div className="exp-title">
-						<h3>Curso de Reparación y Mantenimiento de PC</h3>
-						<p className="exp-date">2013</p>
-					</div>
-					<p className="bajada">Universidad Tecnológica Nacional (UTN)</p>
-				</div>
-				<div className="studies-each right">
-					<div className="exp-title">
-						<h3>CCNA 2 - Protocolos y Conceptos de Ruteo</h3>
-						<p className="exp-date">2012</p>
-					</div>
-					<p className="bajada">CISCO CCNA Exploration</p>
-				</div>
-				<div className="studies-each left">
-					<div className="exp-title">
-						<h3>CCNA 1 - Fundamentos de Redes</h3>
-						<p className="exp-date">2012</p>
-					</div>
-					<p className="bajada">CISCO CCNA Exploration</p>
-				</div>
-				<div className="studies-each right">
-					<div className="exp-title">
-						<h3>Administración de Bases de Datos Informix Dynamic Server</h3>
-						<p className="exp-date">2012</p>
-					</div>
-					<p className="bajada">Universidad Tecnológica Nacional (UTN)</p>
+					<p className="bajada">{t("std5_place")}</p>
 				</div>
 			</div>
 		</section>
